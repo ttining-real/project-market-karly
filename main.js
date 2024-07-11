dda9a44e9e6c790d3fee8ac8851ca6dc3e93ad50
Suubiin <suubiin0819@gmail.com>
2024-07-11 11:10:28 +0900

    🧡 Markup : html 문서 수정
    - product.html: 태그 클래스명 추가
    - header.html: index.html 경로 수정

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 import pb from '@/api/pocketbase';
 import '/src/styles/style.scss';
-import '/src/pages/main/swiper.js'
+import '/src/pages/main/swiper.js';
 
 console.log(pb.collection('products'));
 
