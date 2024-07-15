e331f579c222575c4b6eaeb19e17e7065d5409e4
ttining <ttining.real@gmail.com>
2024-07-15 22:27:12 +0900

    ♻️ Refactor : 회원가입 리팩토링 진행 중 - 아이디/이메일 중복 확인 기능 (미구현) - 이용약관 동의(미구현)

--- src/lib/regExp.js
+++ src/lib/regExp.js
@@ -1,4 +1,10 @@
 /* --------------------------------- 정규 표현식 --------------------------------- */
+// chars (특수문자와 공백)
+export function charsReg(text) {
+  const re = /[\W\s]/;
+  return re.test(String(text));
+}
+
 // idField (최소 4자 ~ 최대 20자, 영문과 숫자를 포함)
 export function idReg(text) {
   const re = /^[a-zA-Z0-9]{4,20}$/;
@@ -11,8 +17,15 @@ export function pwReg(text) {
   return re.test(String(text).toLowerCase());
 }
 
-// chars (특수문자와 공백)
-export function charsReg(text) {
-  const re = /[\W\s]/;
+// nameField (최소 2자 ~ 최대 18자의 한글)
+export function nameReg(text) {
+  const re = /^[가-힣]{2,18}$/;
   return re.test(String(text));
+}
+
+// emailField (@이 포함되어야 함)
+export function emailReg(text) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(text).toLowerCase());
 }
