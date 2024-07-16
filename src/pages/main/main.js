0bd85922d2c7ba0c4ed47f265cc7d325875d2126
rangyeong <rkdud0217@naver.com>
2024-07-17 02:00:24 +0900

    💙 Component : move top button - main페이지에 move top 버튼 추가 - template.html 파일 컴포넌트 생성 - _moveTop.scss파일 생성하고 루트 파일에 추가

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -7,5 +7,6 @@ import '/src/layout/footer/footer.js';
 
 // feature
 import '/src/pages/main/swiper.js';
-import '/src/pages/main/close.js';
+// import '/src/pages/main/close.js';
 import '/src/pages/main/productCard.js';
+import '/src/pages/main/moveTop.js';
--- src/pages/main/moveTop.js
+++ src/pages/main/moveTop.js
@@ -1,4 +1,18 @@
-return(i=Object.assign({method:"GET"},i))
-.query=Object.assign({page:t,perPage:e},i.query),
-this.client.send(this.baseCrudPath,i)
-.then(r=>{var n;return r.items=((n=r.items)==null?void 0:n.map(s=>this.decode(s)))||[],r})}
+document.addEventListener('DOMContentLoaded', function () {
+  const moveTopButton = document.getElementById('move-top-button');
+  let lastScrollTop = 0;
+
+  window.addEventListener('scroll', function () {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+      moveTopButton.classList.add('show');
+      moveTopButton.classList.remove('hide');
+    } else {
+      moveTopButton.classList.remove('show');
+      moveTopButton.classList.add('hide');
+    }
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+  });
+});
