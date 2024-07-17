0492c863931b5827f7a994e7488b7ab1eb139d4a
ttining <ttining.real@gmail.com>
2024-07-17 16:35:30 +0900

    ♻️ Refactor : 회원가입 리팩토링 - 아이디/이메일 중복 확인 기능 추가 - 휴대폰 번호 입력 시 하이픈 자동 입력 기능 추가 - 이용약관 동의 필수 사항 조건 처리

--- src/lib/regExp.js
+++ src/lib/regExp.js
@@ -29,3 +29,8 @@ export function emailReg(text) {
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(text).toLowerCase());
 }
+
+// phoneField
+export function phoneReg(text) {
+  return /[^0-9]/.test(text);
+}
