6efc590fe8cfceb4bbd4c31a2e5f0250dd7330f3
rangyeong <rkdud0217@naver.com>
2024-07-17 12:13:06 +0900

    🎨 Feat : 메인베너 페이지 네이션 💙 Component : top 버튼 컴포넌트 생성 ♻️ Refactor : 상품 리스트 필터 후 출력

--- src/pages/main/productCard.js
+++ src/pages/main/productCard.js
@@ -3,30 +3,35 @@ import '/src/styles/style.scss';
 import getPbImageURL from '@/api/getPbImageURL';
 
 async function renderProductCard() {
-  const productData = await pb.collection('products').getFullList();
+  const allProductData = await pb.collection('products').getFullList();
+  const discountProductData = allProductData.filter(
+    (product) => product.ratio > 0
+  );
   const productLists = document.querySelectorAll(
     '.product-list__swiper .swiper-wrapper'
   );
-  productLists.forEach((productList) => {
-    productData.forEach((product, index) => {
-      const discountPrice =
-        Math.floor((product.price * (1 - product.ratio * 0.01)) / 100) * 100;
 
-      let badgeTemplate = '';
-      if (product.type != 'none') {
-        badgeTemplate += `<span class="badge badge--lg badge--only">${product.type}</span>`;
-      }
-      if (product.benefit != 'none') {
-        const benefits = Array.isArray(product.benefit)
-          ? product.benefit
-          : [product.benefit];
-        benefits.forEach((benefit) => {
-          badgeTemplate += `<span class="badge badge--lg">${benefit}</span>`;
-        });
-      }
+  productLists.forEach((productList, i) => {
+    if (i + 1 === 1) {
+      allProductData.forEach((product, index) => {
+        const discountPrice =
+          Math.floor((product.price * (1 - product.ratio * 0.01)) / 100) * 100;
 
-      const template = product.ratio
-        ? `
+        let badgeTemplate = '';
+        if (product.type != 'none') {
+          badgeTemplate += `<span class="badge badge--lg badge--only">${product.type}</span>`;
+        }
+        if (product.benefit != 'none') {
+          const benefits = Array.isArray(product.benefit)
+            ? product.benefit
+            : [product.benefit];
+          benefits.forEach((benefit) => {
+            badgeTemplate += `<span class="badge badge--lg">${benefit}</span>`;
+          });
+        }
+
+        const template = product.ratio
+          ? `
                       <div class="swiper-slide">
                           <div class="card">
                               <a href="/" class="card__link">
@@ -52,7 +57,7 @@ async function renderProductCard() {
                           </div>
                       </div>
                       `
-        : `
+          : `
                       <div class="swiper-slide">
                           <div class="card">
                               <a href="/" class="card__link">
@@ -77,8 +82,81 @@ async function renderProductCard() {
                       </div>
                       `;
 
-      productList.insertAdjacentHTML('beforeend', template);
-    });
+        productList.insertAdjacentHTML('beforeend', template);
+      });
+    } else if (i + 1 === 2) {
+      discountProductData.forEach((product, index) => {
+        const discountPrice =
+          Math.floor((product.price * (1 - product.ratio * 0.01)) / 100) * 100;
+
+        let badgeTemplate = '';
+        if (product.type != 'none') {
+          badgeTemplate += `<span class="badge badge--lg badge--only">${product.type}</span>`;
+        }
+        if (product.benefit != 'none') {
+          const benefits = Array.isArray(product.benefit)
+            ? product.benefit
+            : [product.benefit];
+          benefits.forEach((benefit) => {
+            badgeTemplate += `<span class="badge badge--lg">${benefit}</span>`;
+          });
+        }
+
+        const template = product.ratio
+          ? `
+                      <div class="swiper-slide">
+                          <div class="card">
+                              <a href="/" class="card__link">
+                                  <img class="card__img" src="${getPbImageURL(product)}" alt="${product.name} 이미지" />
+                                  <div class="card__data">
+                                      <div class="product__info discount product__info--sm">
+                                      <span class="product__info--sm-delivery">샛별배송</span>
+                                      <span class="product__info--sm-title">[${product.brand}] ${product.name}</span>
+                                      <span class="product__info--sm-price discount">${product.price}&nbsp;원</span>
+                                      <div class="product__info--sm-discount discount">
+                                          <span class="product__info--sm-discount-rate">${product.ratio}&#37;<span class="a11y">할인</span></span>
+                                          <span class="product__info--sm-discount-price">${discountPrice}&nbsp;원</span>
+                                      </div>
+                                      </div>
+                                      <div class="badge-container">
+                                      ${badgeTemplate}
+                                      </div>
+                                  </div>
+                              </a>
+                          <button type="button" class="button--cart button button--xs" id='add-cart--${index}'>
+                            <span class="icon icon--cart"></span>담기
+                          </button>
+                          </div>
+                      </div>
+                      `
+          : `
+                      <div class="swiper-slide">
+                          <div class="card">
+                              <a href="/" class="card__link">
+                                  <img class="card__img" src="${getPbImageURL(product)}" alt="${product.name} 이미지" />
+                                  <div class="card__data">
+                                      <div class="product__info discount product__info--sm">
+                                      <span class="product__info--sm-delivery">샛별배송</span>
+                                      <span class="product__info--sm-title">[${product.brand}] ${product.name}</span>
+                                      <div class="product__info--sm-discount discount">
+                                          <span class="product__info--sm-discount-price">${product.price}&nbsp;원</span>
+                                      </div>
+                                      </div>
+                                      <div class="badge-container">
+                                      ${badgeTemplate}
+                                      </div>
+                                  </div>
+                              </a>
+                          <button type="button" class="button--cart button button--xs" id='add-cart--${index}'>
+                            <span class="icon icon--cart"></span>담기
+                          </button>
+                          </div>
+                      </div>
+                      `;
+
+        productList.insertAdjacentHTML('beforeend', template);
+      });
+    }
   });
 }
 
